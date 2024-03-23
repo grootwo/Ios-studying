97221236130331d691b1c19fa9ef1596c3474fa4
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-03-23 18:25:02 +0900

    Make MapView

---
+++ located_mission.swiftpm/Views/MapView.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+import MapKit
+
+struct MapView: View {
+    var coordinate: CLLocationCoordinate2D
+    
+    var body: some View {
+        Map(position: .constant(.region(region)))
+    }
+    
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        )
+    }
+}
