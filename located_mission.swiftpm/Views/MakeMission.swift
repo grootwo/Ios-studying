ceda48c131c4b20481a46619173fc52ba1cc87ee
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-03-25 15:50:44 +0900

    Embed ImagePicker and import it

--- located_mission.swiftpm/Views/MakeMission.swift
+++ located_mission.swiftpm/Views/MakeMission.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct MakeMissionView: View {
-    @State private var tempText: String = ""
+    @State private var inputTitle: String = ""
+    @State private var inputContent: String = ""
+    
+    @State var showImagePicker = false
+    @State var selectedUIImage: UIImage?
+    @State var image: Image?
+    
+    func loadImage() {
+        guard let selectedImage = selectedUIImage else { return }
+        image = Image(uiImage: selectedImage)
+    }
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 30) {
+            if let image = image {
+                            image
+                                .resizable()
+                                .clipShape(Circle())
+                                .frame(width: 120, height: 120)
+                        } else {
+                            Image(systemName: "plus.viewfinder")
+                                .resizable()
+                                .foregroundColor(.blue)
+                                .frame(width: 120, height: 120)
+                        }
+            Button {
+                            showImagePicker.toggle()
+                        } label: {
+                            Text("Image Picker")
+                        }
+                        .sheet(isPresented: $showImagePicker, onDismiss: {
+                            loadImage()
+                        }) {
+                            ImagePicker(image: $selectedUIImage)
+                    }
+            
             Text("새로운 미션 생성하기")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            
+            Text("미션 제목")
+                .font(.title3)
             TextField(
-                "미션 제목을 입력해주세요",
-                text: $tempText
+                "미션 제목을 입력해주세요.",
+                text: $inputTitle,
+                axis: .vertical
+            )
+            Text("미션 내용")
+                .font(.title3)
+            TextField(
+                "미션 내용을 입력해주세요. 미션 내용은 참가자들이 이해할 수 있도록 자세히, 쉽게 설명해주세요.",
+                text: $inputContent,
+                axis: .vertical
             )
         }
         .padding()
