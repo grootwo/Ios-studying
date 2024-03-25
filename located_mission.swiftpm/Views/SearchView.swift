ea0af582f84382b514d1f65d4b2538968500fd06
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-03-25 15:09:39 +0900

    Debug by moving NavigationView code line and edit layout

--- located_mission.swiftpm/Views/SearchView.swift
+++ located_mission.swiftpm/Views/SearchView.swift
@@ -10,51 +10,69 @@ import MapKit
 
 struct SwiftUIView: View {
     var body: some View {
-        ZStack {
-            MapView(coordinate: CLLocationCoordinate2D(latitude: 36.0141447, longitude: 129.3257511))
-            VStack {
-                HStack {
-                    Button(action: {
-                        print("검색 필터 클릭")
-                    }, label: {
-                        Image(systemName: "camera.filters")
-                            .resizable()
-                            .foregroundColor(.mint)
-                            .frame(width: 40, height: 40)
-                    })
+        NavigationView {
+            ZStack {
+                MapView(coordinate: CLLocationCoordinate2D(latitude: 36.0141447, longitude: 129.3257511))
+                VStack {
+                    HStack {
+                        Button(action: {
+                            print("검색 필터 클릭")
+                        }, label: {
+                            Image(systemName: "camera.filters")
+                                .resizable()
+                                .foregroundColor(.mint)
+                                .frame(width: 30, height: 30)
+                        })
+                        .padding()
+                        .frame(width: 70)
+                        .background(Color.white)
+                        .cornerRadius(35)
+                        Spacer()
+                        Button(action: {
+                            print("검색 클릭")
+                        }, label: {
+                            Spacer()
+                            Image(systemName: "magnifyingglass")
+                                .resizable()
+                                .foregroundColor(.mint)
+                                .frame(width: 30, height: 30)
+                        })
+                        .frame(width: 240)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(35)
+                    }
                     .padding()
-                    .frame(width: 70)
-                    .background(Color.white)
-                    .cornerRadius(35)
                     Spacer()
-                    Button(action: {
-                        print("검색 클릭")
-                    }, label: {
-                        Image(systemName: "magnifyingglass")
-                            .resizable()
+                    VStack {
+                        HStack {
+                            Spacer()
+                            NavigationLink(destination: MakeMissionView()) {
+                                Image(systemName: "plus")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                            }
+                            .padding()
+                            .frame(width: 70, height: 70)
                             .foregroundColor(.mint)
-                            .frame(width: 40, height: 40)
-                    })
-                    .frame(width: 240)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(35)
-                }
-                .padding()
-                Spacer()
-                NavigationView {
-                    NavigationLink(destination: MissionListView()) { // 해당 페이지로 넘아가되 화면 전체를 차지하지 않음
+                            .background(Color.white)
+                            .cornerRadius(30)
+                            .offset(y: 50)
+                        }
+                        NavigationLink(destination: MissionListView()) {
                             Text("미션 둘러보기")
                                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        }
+                        .padding()
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        .frame(height: 70)
+                        .foregroundColor(.mint)
+                        .background(Color.white)
+                        .cornerRadius(30)
+                        .offset(y: 60)
                     }
+                    .padding()
                 }
-                .padding()
-                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                .frame(height: 100)
-                .foregroundColor(.mint)
-                .background(Color.white)
-                .cornerRadius(30)
-                .offset(y: 40)
             }
         }
     }
