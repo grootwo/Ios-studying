82c8abec7c7ab0407b0fc4e6c79074736b3aedd5
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-03-26 14:51:29 +0900

    Make one more mission

--- located_mission.swiftpm/Views/MissionListView.swift
+++ located_mission.swiftpm/Views/MissionListView.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct MissionListView: View {
     @State private var missions = [
         Mission(id: 0, isBookmarked: true, titleImageName: "city", userImageName: "hiker", correctImageName: "currynoodle", wrongImageName: "restinside", missionTitle: "대담하게 저녁 식당 탐방하기", location: "일본, 도쿄의 신주쿠구", missionInfo: "지도앱을 보느라 여행 중 풍경을 둘러보지 못해 아쉬웠나요? 수많은 식당들의 수많은 리뷰를 훑느라 지치진 않았나요? 한 번 스마트폰을 끄고 가장 먼저 마주치는 양복을 입은 직장인이 들어가는 식당을 따라 가봐요! 그곳에서 가장 끌리는 음식을 먹고 사진과 함께 후기를 남겨주세요."),
-        Mission(id: 1, isBookmarked: false, titleImageName: "lyingcat", userImageName: "sittingcat", correctImageName: "turtle", wrongImageName: "cigar", missionTitle: "동물들과 함께 쉬기", location: "대한민국, 부산광역시의 초읍어린이대공원", missionInfo: "초읍 어린이 대공원에는 다양한 동물이 많네요. 같이 앉아서 햇빛을 받으며 잠시 늘어져 봅시다! ⛅️")
+        Mission(id: 1, isBookmarked: false, titleImageName: "lyingcat", userImageName: "sittingcat", correctImageName: "turtle", wrongImageName: "cigar", missionTitle: "동물들과 함께 쉬기", location: "대한민국, 부산광역시의 초읍어린이대공원", missionInfo: "초읍 어린이 대공원에는 다양한 동물이 많네요. 같이 앉아서 햇빛을 받으며 잠시 늘어져 봅시다! ⛅️"),
+        Mission(id: 2, isBookmarked: false, titleImageName: "kimchi", userImageName: "emoji", correctImageName: "trash", wrongImageName: "park", missionTitle: "🌳 공원에서 플로깅하기!", location: "대한민국, 부산광역시의 시민공원", missionInfo: "시민공원을 산책하며 바닥의 쓰레기를 정리해봅시다! 할 수 있는 만큼 쓰레기 수집 후 정리한 쓰레기 사진을 올려주세요~!")
     ]
     
     var body: some View {
