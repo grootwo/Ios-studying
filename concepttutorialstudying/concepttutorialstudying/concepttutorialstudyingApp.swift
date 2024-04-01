b5784fa5fcb42414b532001485354a70c0d2ea53
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-01 16:27:16 +0900

    Make concept tutorial studying project

---
+++ concepttutorialstudying/concepttutorialstudying/concepttutorialstudyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  concepttutorialstudyingApp.swift
+//  concepttutorialstudying
+//
+//  Created by Groo on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct concepttutorialstudyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
