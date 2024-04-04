fafd70a2102cfef5775d49654f898e92c6446610
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-04 10:16:48 +0900

    Change images
    Need to debug for caption background

--- concepttutorialstudying/concepttutorialstudying/CaptionedPhoto.swift
+++ concepttutorialstudying/concepttutorialstudying/CaptionedPhoto.swift
@@ -27,20 +27,22 @@ struct Caption: View {
     var body: some View {
         Text(text)
             .padding()
-            .background(Color("TextContrast").opacity(0.75), in: RoundedRectangle(cornerRadius: 15, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+            .background(Color("TextContrast").opacity(0.8), in: RoundedRectangle(cornerRadius: 15, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
             .padding()
     }
 }
 
 struct CaptionedPhoto_Previews: PreviewProvider {
-    static let imageName1 = "shark"
+    static let imageName1 = "Pink_Peony"
     static let imageCaption1 = "This is a cute shark."
-    static let imageName2 = "happycat"
+    static let imageName2 = "Yellow_Daisy"
     static let imageCaption2 = "This is a happy cat."
     static var previews: some View {
         VStack {
             CaptionedPhoto(assetName: imageName1, captionText: imageCaption1)
+                .preferredColorScheme(.dark)
             CaptionedPhoto(assetName: imageName2, captionText: imageCaption2)
+                .preferredColorScheme(.light)
         }
     }
 }
