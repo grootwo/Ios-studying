aaa6832f84cb518e08dbbcf3eccd33dddf7652af
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-04 09:39:22 +0900

    Create KeywordBubbleDefaultPadding.swift
    Without err: Closure containing control flow statement cannot be used with result builder 'ViewBuilder'

---
+++ concepttutorialstudying/concepttutorialstudying/KeywordBubbleDefaultPadding.swift
@@ -0,0 +1,32 @@
+//
+//  KeywordBubbleDefaultPadding.swift
+//  concepttutorialstudying
+//
+//  Created by Groo on 4/4/24.
+//
+
+import SwiftUI
+
+struct KeywordBubbleDefaultPadding: View {
+    let keyword: String
+    let symbol: String
+    var body: some View {
+        Label(keyword, systemImage: symbol)
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue.opacity(0.8), in: Capsule())
+    }
+}
+
+struct KeywordBubbleDefaultPadding_Previews: PreviewProvider {
+    static let keywords = ["Hallo", "Guten Tag", "Bis morgen"]
+    static let symbols = ["figure.wave", "sun.max", "sunrise.fill"]
+    static var previews: some View {
+        VStack {
+            ForEach(symbols, id: \.self) { symbol in
+                KeywordBubbleDefaultPadding(keyword: "Text", symbol: symbol)
+            }
+        }
+    }
+}
