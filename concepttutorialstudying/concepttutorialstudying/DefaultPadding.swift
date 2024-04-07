2ce26a727ee9e7700c4a76fd5841e768b8058ef6
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-07 11:49:09 +0900

    Create DefaultPadding.swift

---
+++ concepttutorialstudying/concepttutorialstudying/DefaultPadding.swift
@@ -0,0 +1,32 @@
+//
+//  DefaultPadding.swift
+//  concepttutorialstudying
+//
+//  Created by Groo on 4/7/24.
+//
+
+import SwiftUI
+
+
+struct DefaultPadding: View {
+    var body: some View {
+        Text("Default Padding")
+        HStack {
+            TrainCar(.rear)
+            TrainCar(.middle)
+                .padding()
+                .background(.green).opacity(0.5)
+            TrainCar(.front)
+        }
+        TrainTrack()
+    }
+}
+
+
+struct DefaultPadding_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            DefaultPadding()
+        }
+    }
+}
