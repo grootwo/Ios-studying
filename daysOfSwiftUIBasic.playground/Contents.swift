f8c7a58ee79f622310c91e6951fb85dedb6b64ec
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-08 16:00:51 +0900

    Write var and let

--- daysOfSwiftUIBasic.playground/Contents.swift
+++ daysOfSwiftUIBasic.playground/Contents.swift
@@ -1,3 +1,17 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let myCatName = "ChoiGo"
+print(myCatName)
+
+// myCatName = "different"
