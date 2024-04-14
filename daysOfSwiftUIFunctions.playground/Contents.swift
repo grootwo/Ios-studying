1855c6f2f40e65ba97200949894d26b22cc96848
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-14 16:32:16 +0900

    Make functions with return

--- daysOfSwiftUIFunctions.playground/Contents.swift
+++ daysOfSwiftUIFunctions.playground/Contents.swift
@@ -1,5 +1,7 @@
 import Cocoa
 
+
+// func
 func waterPlants(plants: [String]) {
     for plant in plants {
         print("Water \(plant)~")
@@ -11,3 +13,22 @@ let plants2 = ["monstera", "cherry blossom"]
 
 waterPlants(plants: plants1)
 waterPlants(plants: plants2)
+
+
+// return
+func getFahrenheit(celsius: Double) -> Double {
+    celsius * 9 / 5 + 32
+}
+
+print(getFahrenheit(celsius: 36.5))
+
+func count10(stop: Int) {
+    for i in 1...10 {
+        if i == stop {
+            return
+        }
+        print(i)
+    }
+}
+
+count10(stop: 4)
