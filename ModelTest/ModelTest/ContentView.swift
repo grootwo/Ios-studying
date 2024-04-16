bd1614e90dc13c066b4a237030db45cc45d3bb63
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-16 23:14:09 +0900

    Create ModelTest

--- ModelTest/ModelTest/ContentView.swift
+++ ModelTest/ModelTest/ContentView.swift
@@ -7,21 +7,23 @@
 
 import SwiftUI
 
+struct Diary {
+    var date: String
+    var weather: String
+    var title: String
+    var content: String
+    var hide: Bool = false
+}
+
 struct ContentView: View {
+    @State var diaries: [Diary] = [
+        Diary(date: "4월 15일", weather: "cloud", title: "오늘의 하루", content: "오늘 날이 흐려서 책을 읽었다."),
+        Diary(date: "4월 16일", weather: "cloud", title: "오늘의 하루", content: "오늘 날이 흐려서 책을 읽었다.", hide: true),
+        Diary(date: "4월 17일", weather: "cloud", title: "오늘의 하루", content: "오늘 날이 흐려서 책을 읽었다."),
+    ]
     var body: some View {
         VStack {
-            List {
-                ForEach(0..<5) { _ in
-                    if true {
-                        VStack(alignment: .leading) {
-                            Text("4월 15일")
-                            Image(systemName: "cloud")
-                            Text("오늘의 하루")
-                            Text("오늘 날이 흐려서 책을 읽었다.")
-                        }
-                    }
-                }
-            }
+            List {            }
         }
         .padding()
     }
---
+++ ModelTest/ModelTest/ModelTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelTestApp.swift
+//  ModelTest
+//
+//  Created by Groo on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ModelTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
