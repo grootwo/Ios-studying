537d08138a3ca2b0aa1f442992d57e3c768ce4ac
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-16 23:15:36 +0900

    Create StateBindingTestApp.swift

---
+++ StateBindingTest/StateBindingTest/StateBindingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingTestApp.swift
+//  StateBindingTest
+//
+//  Created by Groo on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
