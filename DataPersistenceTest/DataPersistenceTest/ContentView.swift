de8c0aad8a7437f3e4a7a015d8f1827124c2664a
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-16 16:10:24 +0900

    Create ContentView.swift

---
+++ DataPersistenceTest/DataPersistenceTest/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DataPersistenceTest
+//
+//  Created by Groo on 4/16/24.
+//  keyword: Create Read Update Delete
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
