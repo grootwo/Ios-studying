f6e732c64800182b90cdc66be08e848776d1fb59
soaringwave <82134672+soaringwave@users.noreply.github.com>
2024-04-17 18:15:24 +0900

    Delete one line

--- FilterSortMap.playground/Contents.swift
+++ FilterSortMap.playground/Contents.swift
@@ -5,4 +5,3 @@ let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 // print(luckyNumbers.filter({ $0 % 2 != 0 }))
 // print(luckyNumbers.filter({ $0 % 2 != 0 }).sorted(by: { $0 < $1 }))
 luckyNumbers.filter({ $0 % 2 != 0 }).sorted(by: { $0 < $1 }).map({ print("\($0) is a lucky number") })
-
