a352549846c13b2fc909232b15beaa97b1b04559
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-04 14:56:50 +0900

    Use Views as properties

--- daysofSwiftUIViewsAndModifiers/daysofSwiftUIViewsAndModifiers/ContentView.swift
+++ daysofSwiftUIViewsAndModifiers/daysofSwiftUIViewsAndModifiers/ContentView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showRedBackground = false
+    var text1: some View {
+        Text("this is text1")
+    }
+    let text2 = Text("this is text2")
+    var text3: some View {
+        Group {
+            Text("this is text3")
+            Text("this is text3")
+        }
+    }
+    @ViewBuilder var text4: some View {
+        Text("this is text4")
+        Text("this is text4")
+    }
     var body: some View {
-        Button(action: {
-            showRedBackground.toggle()
-        }, label: {
-            Text("This is a Text")
-        })
-            .font(.largeTitle)
-            .foregroundColor(showRedBackground ? .red : .green)
+        text1
+        text2
+        text3
+        text4
     }
 }
 
