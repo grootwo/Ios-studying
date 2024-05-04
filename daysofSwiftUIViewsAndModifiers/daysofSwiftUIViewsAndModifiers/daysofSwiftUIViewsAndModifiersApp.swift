aaea56c3a7a436d4218a714bdbafc21efb24aa84
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-04 13:52:34 +0900

    Create ViewsAndModifiers project

---
+++ daysofSwiftUIViewsAndModifiers/daysofSwiftUIViewsAndModifiers/daysofSwiftUIViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  daysofSwiftUIViewsAndModifiersApp.swift
+//  daysofSwiftUIViewsAndModifiers
+//
+//  Created by Groo on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct daysofSwiftUIViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
