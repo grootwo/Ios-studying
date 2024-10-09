5d4130ca54d64cae3d0208c44aa300f52a44a83d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-09 16:14:30 +0900

    Feat: create TimerTest project

---
+++ TimerTest/TimerTest/TimerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTestApp.swift
+//  TimerTest
+//
+//  Created by Groo on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
