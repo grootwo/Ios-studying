95906640e97b50b125f8c5b4be85f75191431131
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-09 16:52:43 +0900

    Feat: add 2 timers with .relative and background

--- TimerTest/TimerTest/ContentView.swift
+++ TimerTest/TimerTest/ContentView.swift
@@ -11,27 +11,33 @@ struct ContentView: View {
     @State private var isTicking = false
     @State private var timeCounting = 0
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    let now = Date.now
     var body: some View {
-        VStack {
-            Text("\(timeCounting)")
-                .font(.largeTitle)
-                .onReceive(timer) { time in
-                    if isTicking {
-                        timeCounting += 1
+        VStack(spacing: 50) {
+            VStack {
+                Text("\(timeCounting)")
+                    .font(.largeTitle)
+                    .onReceive(timer) { time in
+                        if isTicking {
+                            timeCounting += 1
+                        }
                     }
+                Button(isTicking ? "Pause" : "Start") {
+                    isTicking.toggle()
                 }
-            Button(isTicking ? "Pause" : "Start") {
-                isTicking.toggle()
+                .font(.title)
+                .buttonStyle(.borderedProminent)
+                Button("Stop") {
+                    isTicking = false
+                    timeCounting = 0
+                }
+                .font(.title)
+                .buttonStyle(.borderedProminent)
+                .tint(.secondary)
             }
-            .font(.title)
-            .buttonStyle(.borderedProminent)
-            Button("Stop") {
-                isTicking = false
-                timeCounting = 0
-            }
-            .font(.title)
-            .buttonStyle(.borderedProminent)
-            .tint(.secondary)
+            BackgroundTimer()
+            Text(now, style: .relative)
+                .font(.title)
         }
         .padding()
     }
