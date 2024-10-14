dd944d05e07e3da51394ee51b9592f34c42c67e3
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-10-14 16:50:36 +0900

    Feat: create swiftui file

---
+++ ImessageStudy/ImessageStudy MessagesExtension/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  ImessageStudy MessagesExtension
+//
+//  Created by Groo on 10/14/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
